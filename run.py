d9e9283740a392a6830b302d1bc19ae40deca521
cjb <chejingbo@ynlcgroup.com>
2020-12-24 18:25:27 +0800

    新增 1.修改报告中设备中文名称 2.备份报告 3.修改报告中的路径 4.上传报告至服务器

--- run.py
+++ run.py
@@ -10,6 +10,9 @@ import shutil
 from airtest.core.android.adb import ADB
 from jinja2 import Environment, FileSystemLoader
 import sendmail
+import xlrd
+import paramiko
+from bs4 import BeautifulSoup
 
 
 def run(devices, air, run_all=False):
@@ -20,6 +23,8 @@ def run(devices, air, run_all=False):
     """
     file = os.getcwd() + '\\' + air
     json_file = os.path.join(file, 'data.json')
+    project_name = os.getcwd().split('\\')[1]
+    romote = "/home/autotest" + '/' +project_name
     try:
         results = load_json_data(air, run_all)
         tasks = run_on_multi_device(devices, air, results, run_all)
@@ -29,6 +34,9 @@ def run(devices, air, run_all=False):
             results['tests'][task['dev']]['status'] = status
             json.dump(results, open(json_file, "w"), indent=4)
         run_summary(results)
+        ftp_dir = copy_log(air)
+        change_url(devices, air, ftp_dir)
+        sftp_upload(ftp_dir, romote, air)
     except Exception as e:
         traceback.print_exc()
 
@@ -86,15 +94,36 @@ def run_one_report(air, dev):
                 "--log_root",
                 log_dir,
                 "--outfile",
-                os.path.join(log_dir, now + 'log.html'),
+                os.path.join(log_dir, 'log.html'),
                 "--lang",
                 "zh"
             ]
             ret = subprocess.call(cmd, shell=True, cwd=os.getcwd())
-
+            datapath = os.path.join(os.getcwd(), "device.xls")
+            data = xlrd.open_workbook(datapath)
+            # 读取第一个工作表
+            table = data.sheets()[0]
+            # 统计行数
+            rows = table.nrows
+            data = []  # 存放数据
+            for i in range(1, rows):
+                values = table.row_values(i)
+                data.append(
+                    (
+                        {
+                            "code": values[1],
+                            "name": values[0],
+                        }
+                    )
+                )
+            # print(data)
+            for i in range(0, len(data)):
+                if dev == data[i]["code"]:
+                    name = data[i]["name"]
             return {
                 'status': ret,
-                'path': os.path.join(log_dir_new, now + 'log.html')
+                'path': os.path.join(log_dir_new, 'log.html'),
+                'name': name
             }
         else:
             print("Report build Failed. File not found in dir %s" % log)
@@ -118,6 +147,7 @@ def run_summary(data):
         summary.update(data)
         summary['start'] = time.strftime("%Y-%m-%d %H:%M:%S",
                                          time.localtime(data['start']))
+        print(summary)
         env = Environment(loader=FileSystemLoader(os.getcwd()),
                           trim_blocks=True)
         html = env.get_template('report_tpl.html').render(data=summary)
@@ -176,6 +206,113 @@ def get_log_dir(device, air):
     return log_dir
 
 
+def copy_log(air):
+    """
+    创建报告的副本，用来修改报告文件中的参数
+    """
+    now = time.strftime('%Y-%m-%d_%H_%M_%S')
+    log_dir = os.path.join(os.getcwd(), air, 'log')
+    ftp_dir = os.path.join(os.getcwd(), "FTP_report", air, "report" + now)
+    report_dir = os.path.join(os.getcwd(), 'summary_report')
+    list = os.listdir(report_dir)
+    list.sort(key=lambda fn: os.path.getmtime(report_dir + '\\' + fn))
+    report = os.path.join(report_dir, list[-1])
+    # if not os.path.exists(ftp_dir):
+    #     os.makedirs(ftp_dir)
+    # for log_dir_1 in  os.listdir(log_dir):
+    #     path = os.path.join(log_dir, log_dir_1)
+
+    shutil.copytree(log_dir, ftp_dir)
+    shutil.copy(report, ftp_dir)
+    return ftp_dir
+
+
+def change_url(device, air, ftp):
+    global solo_report
+    project_name = os.getcwd().split('\\')[1]
+    folder_name = ftp.split('\\')[-1]
+    file_data = " "
+    file_data_1 = " "
+    sum_report = ftp + "\\" + air + "_report.html"
+    for dev in device:
+        solo_report = ftp + "\\" + dev + "\\" + "log.html"
+
+    old_str = r"D:\multi-device-runner-master\test_oppo.air\log"
+    old_str_1 = r"c:/users/c/appdata/local/programs/python/python37/lib/site-packages/airtest"
+    old_str_2 = r"D:\\multi-device-runner-master\\test_oppo.air\\log"
+    old_str_3 = r"c://users//c//appdata//local//programs//python//python37//lib//site-packages//airtest"
+    new_str = "http://47.108.203.157:15000/" + r"/" + project_name + r"/" + air + r"/" + folder_name
+    new_str_1 = r"http://47.108.203.157:15000/airtest"
+    with open(sum_report, "r", encoding="utf-8") as f:
+        for line in f:
+            if old_str in line:
+                line = line.replace(old_str, new_str)
+                line = line.replace("\\", "/")
+            file_data += line
+    with open(sum_report, "w", encoding="utf-8") as f:
+        f.write(file_data)
+    if os.path.exists(solo_report):
+        with open(solo_report, "r", encoding="utf-8") as p:
+            for line1 in p:
+                if old_str_1 in line1:
+                    line1 = line1.replace(old_str_1, new_str_1)
+                    # line1 = line1.replace("\\", "/")
+                if old_str in line1:
+                    line1 = line1.replace(old_str, new_str)
+                    line1 = line1.replace("\\", "/")
+                if old_str_2 in line1:
+                    line1 = line1.replace(old_str_2, new_str)
+                if old_str_3 in line1:
+                    line1 = line1.replace(old_str_3, new_str_1)
+                file_data_1 += line1
+        with open(solo_report, "w", encoding="utf-8") as p:
+            p.write(file_data_1)
+    else:
+        print(solo_report)
+
+
+def sftp_upload( local, remote, air):
+    host = "47.108.203.157"
+    port  = 22
+    username='autotest'
+    pwd='12345,Asdf'
+    folder_name = local.split('\\')[-1]
+    sf = paramiko.Transport((host, port))
+    sf.connect(username=username, password=pwd)
+    sftp = paramiko.SFTPClient.from_transport(sf)
+
+    try:
+        for f in os.listdir(local):
+            # 遍历本地目录
+            local_1 = local + "\\" + f
+            if os.path.isdir(local_1):
+                fwq_dir = remote+ '/' + air
+                fwq_dir_1 = remote + '/' + air + '/' + folder_name
+                fwq_dir_2 = fwq_dir_1 + '/' + f
+                # if os.path.exists(fwq_dir):
+                #     continue
+                # else:
+                #     sftp.mkdir(fwq_dir)
+                # if not os.path.exists(fwq_dir):
+                #     sftp.mkdir(fwq_dir)
+                if not os.path.exists(fwq_dir_1):
+                    sftp.mkdir(fwq_dir_1)
+                        # sftp.mkdir(os.path.split(fwq_dir)[1])
+                if not os.path.exists(fwq_dir_2):
+                    sftp.mkdir(fwq_dir_2)
+            if os.path.isdir(local_1):
+                for p in os.listdir(local_1):
+                    upload_file = os.path.join(local_1, p)
+                    fwq_dir_3 = fwq_dir_2 + '/' + p
+                    sftp.put(upload_file, fwq_dir_3)  # 上传目录中的文件
+            else:
+                fwq_dir_4 = fwq_dir_1 + '/' + f
+                sftp.put(local_1, fwq_dir_4)
+    except Exception as e:
+        print('upload exception:', e)
+    sf.close()
+
+
 if __name__ == '__main__':
     """
         初始化数据
@@ -183,10 +320,20 @@ if __name__ == '__main__':
     """
     devices = [tmp[0] for tmp in ADB().devices()]
     airs = os.listdir(os.getcwd())
+
+    # with open(report, "r", encoding="utf-8") as f:
+    #     f.read()
+    #     print(f)
+    # soup = BeautifulSoup(open(report), features='html.parser')
+    # print(soup)
+    # for tag in soup.find_all('D:\multi-device-runner-master'):
+    #     print(tag)
+
     for air in airs:
         if '.air' in air:
             run(devices, air, run_all=True)
+
             mail = sendmail.SendMail()
-            mail.send()
+            mail.send(air)
         else:
             print('没有找到测试脚本')
