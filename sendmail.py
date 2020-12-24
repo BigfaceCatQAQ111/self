ee72bd8ce750559748cc0254039c386c7aa99e47
cjb <chejingbo@ynlcgroup.com>
2020-12-24 18:26:18 +0800

    修改发送报告路径

--- sendmail.py
+++ sendmail.py
@@ -9,7 +9,7 @@ from email.mime.multipart import MIMEMultipart
 # from config import globalparam
 
 # 测试报告的路径
-report_dir = os.path.join(os.getcwd(), 'summary_report')
+
 project_name = os.getcwd().split('\\')[1]
 
 # 配置收发件人
@@ -26,35 +26,55 @@ class SendMail:
 		else:
 			self.sendTo = recver
 
-	def __get_report(self):
+	def __get_report(self,air):
 		"""获取最新测试报告"""
+		report_dir = os.path.join(os.getcwd(), 'FTP_report',air)
 		list = os.listdir(report_dir)
 		list.sort(key=lambda fn: os.path.getmtime(report_dir + '\\' + fn))
 		report = os.path.join(report_dir, list[-1])
-		print('The new report name: {0}'.format(report))
-		return report
+		report_to_send = report+"\\" + air + "_report.html"
+		print('The new report name: {0}'.format(report_to_send))
+		return report_to_send
 
-	def __take_messages(self):
+	def __take_messages(self,air):
 		"""生成邮件的内容，和html报告附件"""
-		newreport = self.__get_report()
+		newreport = self.__get_report(air)
+		report_dir = os.path.join(os.getcwd(), 'FTP_report', air)
+		list = os.listdir(report_dir)
+		list.sort(key=lambda fn: os.path.getmtime(report_dir + '\\' + fn))
+		report = os.path.join(report_dir, list[-1])
+		dir = report.split("\\")[-1]
+		url = "http://47.108.203.157:15000/multi-device-runner-master/" + '/' + air + '/' + dir + "/" + air + "_report.html"
 		self.msg = MIMEMultipart()
 		self.msg['Subject'] = project_name
 		self.msg['date'] = time.strftime('%a, %d %b %Y %H:%M:%S %z')
+		self.msg['url'] = url
 
 		with open(os.path.join(newreport), 'rb') as f:
 			mailbody = f.read()
 		html = MIMEText(mailbody, _subtype='html', _charset='utf-8')
 		self.msg.attach(html)
-		
+
+		# report_dir = os.path.join(os.getcwd(), 'FTP_report', air)
+		# list = os.listdir(report_dir)
+		# list.sort(key=lambda fn: os.path.getmtime(report_dir + '\\' + fn))
+		# report = os.path.join(report_dir, list[-1])
+		# dir = report.split("\\")[-1]
+		# url = "http://47.108.203.157:15000/multi-device-runner-master/" + '/' + air + '/'+  dir + "/"+ air+"_report.html"
+		# att = MIMEText(url, 'base64', 'utf-8')
+		# att["Content-Type"] = 'application/octet-stream'
+		# att['Content-Disposition'] = 'attachment;访问连接="%s"' % (url)
+		# self.msg.attach(att)
+
 		# html附件
 		att1 = MIMEText(mailbody, 'base64', 'gb2312')
 		att1["Content-Type"] = 'application/octet-stream'
 		att1["Content-Disposition"] = 'attachment; filename="TestReport.html"'#这里的filename可以任意写，写什么名字，邮件中显示什么名字
 		self.msg.attach(att1)
 
-	def send(self):
+	def send(self,air):
 		"""发送邮件"""
-		self.__take_messages()
+		self.__take_messages(air)
 		self.msg['from'] = sendaddr_name
 		try:
 			smtp = smtplib.SMTP('smtp.exmail.qq.com',25)
